c6a2efbf614e34297d886aee37b8ab6ef950567a
caporatang <caporatang@naver.com>
2024-08-18 21:41:41 +0900

    Feat : UserRepository 간단한 테스트 추가

---
+++ test-code-with-architecture/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * packageName : com.example.demo.repository
+ * fileName : UserRepositoryTest
+ * author : taeil
+ * date : 8/18/24
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 8/18/24        taeil                   최초생성
+ */
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+}
