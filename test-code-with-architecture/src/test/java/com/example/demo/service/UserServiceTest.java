2077477a0648bcdb8cd86d290331ccb4edeb85f1
caporatang <caporatang@naver.com>
2024-08-19 22:15:38 +0900

    Add : UserService 테스트 메서드 추가

--- test-code-with-architecture/src/test/java/com/example/demo/service/UserServiceTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,16 +1,28 @@
 package com.example.demo.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * packageName : com.example.demo.service
@@ -27,15 +39,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 @TestPropertySource("classpath:test-application.properties")
 @SqlGroup({
-        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 })
-@Sql("/sql/user-service-test-data.sql")
 public class UserServiceTest {
 
     @Autowired
     private UserService userService;
 
+    @MockBean
+    private JavaMailSender mailSender;
 
     @Test
     void getByEmail은_ACTIVE_상태인_유저_찾아올_수_있다() {
@@ -71,6 +84,83 @@ public class UserServiceTest {
         }).isInstanceOf(ResourceNotFoundException.class);
     }
 
+    @Test
+    void userCreateDto_를_이용하여_유저를_생성할_수_있다() {
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("caporatang3333@naver.com")
+                .address("Gyeongi")
+                .nickname("caporatang3333")
+                .build();
+
+        // 메일 발송 부분 강의 내용과 다른 이슈가 있어 mock 설정을 다르게..
+        // MimeMessage Mock 객체 생성 및 설정
+        MimeMessage mimeMessage = Mockito.mock(MimeMessage.class);
+        BDDMockito.given(mailSender.createMimeMessage()).willReturn(mimeMessage);
+
+        // MimeMessageHelper 내부에서 사용하는 mailSender.send()가 호출될 때 아무 동작도 하지 않도록 설정
+        BDDMockito.doNothing().when(mailSender).send(any(MimeMessage.class));
+
+        // when
+        UserEntity result = userService.create(userCreateDto);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
+        // assertThat(result.getCertificationCode()).isEqualTo("T.T"); // FIXME
+    }
+
+    @Test
+    void userCreateDto_를_이용하여_유저를_수정할_수_있다() {
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("Gyeongi")
+                .nickname("caporatang3334")
+                .build();
+
+        // when
+        UserEntity result = userService.update(3, userUpdateDto);
+
+        // then
+        UserEntity userEntity = userService.getById(3);
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getAddress()).isEqualTo("Gyeongi");
+        assertThat(userEntity.getNickname()).isEqualTo("caporatang3334");
+    }
+
+
+    @Test
+    void user를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+        // given
+        //when
+        userService.login(3);
+
+        // then
+        UserEntity userEntity = userService.getById(3);
+        assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+        // assertThat(result.getLastLoginAt()).isEqualTo("T.T"); // FIXME
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() {
+        // given
+        //when
+        userService.verifyEmail(2,"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+
+        // then
+        UserEntity userEntity = userService.getById(2);
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            userService.verifyEmail(2,"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac");
+        }).isInstanceOf(CertificationCodeNotMatchedException.class);
+    }
 
 
 
--- test-code-with-architecture/src/test/resources/sql/user-service-test-data.sql
+++ test-code-with-architecture/src/test/resources/sql/user-service-test-data.sql
@@ -1,5 +1,5 @@
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (1, 'caporatang@naver.com', 'capo', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+values (3, 'caporatang@naver.com', 'capo', 'jeonju', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
 
 insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 values (2, 'caporatang2@naver.com', 'capoo', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
