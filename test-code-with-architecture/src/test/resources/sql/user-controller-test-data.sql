39a3f1bfe588783cc5863eb2560832e91e9aaec0
caporatang <caporatang@naver.com>
2024-08-19 23:01:30 +0900

    Add : UserController 테스트 추가

---
+++ test-code-with-architecture/src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (3, 'caporatang@naver.com', 'capo', 'jeonju', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'caporatang2@naver.com', 'capoo', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
