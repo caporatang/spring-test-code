f003f5e649e643f80a7432a9c9c0e68f8236498b
caporatang <caporatang@naver.com>
2024-08-20 19:05:53 +0900

    Refactor : 불필요한 service 추상화 제거

--- test-code-with-architecture/src/main/java/com/example/demo/user/service/UserServiceImpl.java
+++ test-code-with-architecture/src/main/java/com/example/demo/user/service/UserServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Builder
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService, UserCreateService, UserUpdateService, UserReadService, AuthenticationService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final CertificationService certificationService;
